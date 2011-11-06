bf5351dc918624d98e805de6041749eff41d3aec
byakuya@bigmir.net <byakuya@bigmir.net@846e4283-28e6-861e-26fc-b5a983cf3f06>
2011-11-06 19:29:26 +0000

    Добавлен черновой проект для определения и задания условий данных,а именно: -создана форма для тестирования проекта -создан клас Parser, в нем реализован поиск и сохранение условий полученых с интерфейса

---
+++ trunk/tl410-rias  v. 1.0/Conditioner/Conditioner/Conditioner/Form1.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Conditioner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string res="";
+            Parser pr = new Parser("(((hight>3)&&(time==15))||(distance<45))");
+            List<string> li;
+            li = pr.getList();
+            foreach (string str in li)
+            {
+                res += str + @"
+";
+            }
+            textBox2.Text = res;
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ trunk/tl410-rias  v. 1.0/Conditioner/Conditioner/Conditioner/Parser.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Conditioner
+{
+    class Parser
+    {
+        private String conditions;
+        private int condtionsNesting;
+        private int currentNesting = 0;
+        private int [] [] subMass;
+        //private string [][] parsedConditions;
+        private List<string> findedCondions = new List<string>();
+        private Dictionary<string, string[][]> parsedConditions = new Dictionary<string, string[][]>();
+        
+        public Parser(string conditions)
+        {
+            this.conditions = conditions; //получаем строку условий
+            parseConditions();            //парсим строку
+
+
+            findConditions();
+        }
+
+        private void parseConditions() 
+        {
+            
+            for (int i = 0; i < conditions.Length;i++)
+            {
+                /*switch (conditions[i])
+                {
+                    case '(': 
+                        currentNesting++;
+                        break;
+                    case 
+                }*/
+            }
+
+        }
+
+        private void findConditions()
+        {
+            int startPosSub = 0;
+            for (int i = 0; i < conditions.Length; i++)
+            {
+
+                if (conditions[i]=='(')
+                {
+                    if (conditions[i+1] != '(')            // если встречаем открывающие скобки, проверяем не идет ли впереди
+                    {                                      // еще 1 скобка. Если нет то запоминаем позицию.
+                        startPosSub = i; 
+                    }
+                }
+                if (conditions[i] == ')')
+                {
+                    if (conditions[i - 1] != ')')          // если встретили закрывающую скобку и перед ней нету еще 1, то берем
+                    {                                      // рание сохранненную позицую и делаем сабстринг, таким образом занося 
+                        findedCondions.Add(conditions.Substring(startPosSub+1, i- startPosSub-1 ).ToString());     // условие в список
+                    }
+                }
+                if ((conditions[i] == '|') || (conditions[i] == '&'))  // если встретили один из знаков проверяем нет ли перед ним такого
+                {                                                      // же знака, если нету то сохраняем его как условие
+                    if((conditions[i-1] != '|') && (conditions[i-1] != '&'))
+                    {
+                    findedCondions.Add(conditions.Substring(i,1));
+                    }
+                }/*
+                if (conditions[i] == '=')
+                {
+
+                }
+                if (conditions[i] == '!')
+                {
+
+                }
+                if ((conditions[i] == '<') || (conditions[i] == '>'))
+                {
+
+                }
+                else 
+                {
+ 
+                }*/
+            }
+                
+           }
+
+        public List<string> getList()
+        {
+            return findedCondions;
+
+        } 
+        }
+
+   }
+

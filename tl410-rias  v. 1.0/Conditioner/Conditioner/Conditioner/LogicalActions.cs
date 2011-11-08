8d68b3ee55fba1b2ddde1f4a348e1f30b0c742bc
byakuya@bigmir.net <byakuya@bigmir.net@846e4283-28e6-861e-26fc-b5a983cf3f06>
2011-11-08 20:45:13 +0000

    Добавлены изменения в проект для определения и задания условий данных: -добавлен класс LogicalActions, который отвечает за выполнения логических условий и сравнений -добавлены методы парсинга внутренних условий в классе Parser

--- tl410-rias  v. 1.0/Conditioner/Conditioner/Conditioner/LogicalActions.cs
+++ tl410-rias  v. 1.0/Conditioner/Conditioner/Conditioner/LogicalActions.cs
@@ -3,10 +3,37 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
-namespace Conditioner
+namespace ConsoleApplication1
 {
     class LogicalActions
     {
-       
+        public bool moreThen(double permitValue, double resValue)
+        {
+            return permitValue < resValue;
+        }
+        public bool lessThen(double permitValue, double resValue)
+        {
+            return permitValue > resValue;
+        }
+        public bool moreEqThen(double permitValue, double resValue) 
+        {
+            return permitValue >= resValue;
+        }
+        public bool lessEqThen(double permitValue, double resValue) 
+        {
+            return permitValue <= resValue;
+        }
+        public bool equalent(double permitValue, double resValue) 
+        {
+            return permitValue == resValue;
+        }
+        public bool and(bool firstExpRes, bool secondExpRes)
+        {
+            return firstExpRes && secondExpRes;
+        }
+        public bool or(bool firstExpRes, bool secondExpRes)
+        {
+            return firstExpRes || secondExpRes;
+        }
     }
 }
--- tl410-rias  v. 1.0/Conditioner/Conditioner/Conditioner/Parser.cs
+++ tl410-rias  v. 1.0/Conditioner/Conditioner/Conditioner/Parser.cs
@@ -10,11 +10,11 @@ namespace Conditioner
         private String conditions;
         private int condtionsNesting;
         private int currentNesting = 0;
-        private int [] [] subMass;
+        private int[][] subMass;
         //private string [][] parsedConditions;
         private List<string> findedCondions = new List<string>();
         private Dictionary<string, string[][]> parsedConditions = new Dictionary<string, string[][]>();
-        
+
         public Parser(string conditions)
         {
             this.conditions = conditions; //получаем строку условий
@@ -23,11 +23,13 @@ namespace Conditioner
 
             findConditions();
         }
-
-        private void parseConditions() 
+        public Parser()
         {
-            
-            for (int i = 0; i < conditions.Length;i++)
+        }
+        private void parseConditions()
+        {
+
+            for (int i = 0; i < conditions.Length; i++)
             {
                 /*switch (conditions[i])
                 {
@@ -46,25 +48,25 @@ namespace Conditioner
             for (int i = 0; i < conditions.Length; i++)
             {
 
-                if (conditions[i]=='(')
+                if (conditions[i] == '(')
                 {
-                    if (conditions[i+1] != '(')            // если встречаем открывающие скобки, проверяем не идет ли впереди
+                    if (conditions[i + 1] != '(')            // если встречаем открывающие скобки, проверяем не идет ли впереди
                     {                                      // еще 1 скобка. Если нет то запоминаем позицию.
-                        startPosSub = i; 
+                        startPosSub = i;
                     }
                 }
                 if (conditions[i] == ')')
                 {
                     if (conditions[i - 1] != ')')          // если встретили закрывающую скобку и перед ней нету еще 1, то берем
                     {                                      // рание сохранненную позицую и делаем сабстринг, таким образом занося 
-                        findedCondions.Add(conditions.Substring(startPosSub+1, i- startPosSub-1 ).ToString());     // условие в список
+                        findedCondions.Add(conditions.Substring(startPosSub + 1, i - startPosSub - 1).ToString());     // условие в список
                     }
                 }
                 if ((conditions[i] == '|') || (conditions[i] == '&'))  // если встретили один из знаков проверяем нет ли перед ним такого
                 {                                                      // же знака, если нету то сохраняем его как условие
-                    if((conditions[i-1] != '|') && (conditions[i-1] != '&'))
+                    if ((conditions[i - 1] != '|') && (conditions[i - 1] != '&'))
                     {
-                    findedCondions.Add(conditions.Substring(i,1));
+                        findedCondions.Add(conditions.Substring(i, 1));
                     }
                 }/*
                 if (conditions[i] == '=')
@@ -84,15 +86,32 @@ namespace Conditioner
  
                 }*/
             }
-                
-           }
+
+        }
+
+        public string findVeriable(string expression)
+        {
+            for (int i = 0; i < expression.Length; i++)
+            {
+                if ((expression[i + 1] == '>') && (expression[i + 1] == '=') && (expression[i + 1] == '<'))
+                {
+                    return expression.Substring(0, i);
+                }
+            }
+            return "Veriable Not Found";
+        }
+        public string findAction(string expression)
+        {
+            return "";
+ 
+        }
 
         public List<string> getList()
         {
             return findedCondions;
 
-        } 
         }
+    }
 
-   }
+}
 

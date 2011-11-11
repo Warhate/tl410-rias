80aefc3ae534a5ee6f823ae8b8259cfbbd590b4f
IllyaShveda@gmail.com <IllyaShveda@gmail.com@846e4283-28e6-861e-26fc-b5a983cf3f06>
2011-11-11 19:27:56 +0000

    Добавлена функция записи получаемых параметров в текстовый файл Добавлено время передачи параметров

---
+++ trunk/tl410-rias  v. 1.0/Loger/Logsaver/LogStorage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+namespace Logsaver
+{
+    public abstract class LogStorage
+    {
+        private string Log;
+        public string getLog()
+        {
+            return Log;
+        }
+        public void setLog(string newLog)
+        {
+            Log = System.DateTime.Now + " " + newLog + "\r\n";
+        }
+        public void saveStr()
+        {
+            StreamWriter SW = new StreamWriter(new FileStream("Log.txt", FileMode.Append, FileAccess.Write));
+            SW.Write(getLog());
+            SW.Close();
+        }
+    }
+}
